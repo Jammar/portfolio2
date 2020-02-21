8d9ba2953fb309f67040654a2f52beafcd1a7202
Jammar <46968969+Jammar@users.noreply.github.com>
2020-02-21 14:34:29 +0100

    temp removing Socials and not-used graphql

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,7 +7,7 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
+// import { useStaticQuery, graphql } from "gatsby"
 
 // import MainMenu from "./MainMenu"
 import "./normalize.css"
@@ -15,15 +15,15 @@ import "./layout.css"
 import "./media.css"
 
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
+  // const data = useStaticQuery(graphql`
+  //   query SiteTitleQuery {
+  //     site {
+  //       siteMetadata {
+  //         title
+  //       }
+  //     }
+  //   }
+  // `)
 
   return (
     <>
