50a6b9097ba6d101367ea38efb0e4bee4b367079
Jammar <46968969+Jammar@users.noreply.github.com>
2020-02-24 18:25:04 +0100

    fixed Socials component to match rest of build

--- src/components/socials.js
+++ src/components/socials.js
@@ -2,25 +2,21 @@ import React from 'react'
 import { IoLogoGithub } from 'react-icons/io';
 import { MdEmail } from 'react-icons/md';
 
-class Socials extends React.Component {
-    render() {
-        return (
-            <div className="socials">
-                <ul className="icons">
-                    <li>
-                        <a href="https://github.com/Jammar">
-                            <IoLogoGithub />
-                        </a>
-                    </li>
-                    <li>
-                        <a href="mailto:joachim@hideaddress.net">
-                            <MdEmail />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        )
-    }
-}
+const Socials = () => (
+    <div className="socials">
+        <ul className="icons">
+            <li>
+                <a href="https://github.com/Jammar">
+                    <IoLogoGithub />
+                </a>
+            </li>
+            <li>
+                <a href="mailto:joachim@hideaddress.net">
+                    <MdEmail />
+                </a>
+            </li>
+        </ul>
+    </div>
+)
 
 export default Socials
