931b9ae78fdf5c7f6879854c2ae55f76513856da
Jammar <46968969+Jammar@users.noreply.github.com>
2020-02-25 12:27:12 +0100

    trying Socials

--- src/components/socials.js
+++ src/components/socials.js
@@ -1,22 +1,20 @@
 import React from "react"
 
-class Socials extends React.Component {
-    render() {
-        return <div className="socials">
-            <ul className="icons">
-                <li>
-                    <a href="https://github.com/Jammar" className="fa fa-github" alt="Link to github page">
-                        <span className="label">Link to github page</span>
-                    </a>
-                </li>
-                <li>
-                    <a href="mailto:joachim@hideaddress.net" className="fa fa-at" alt="Mailto-link to author">
-                        <span className="label">Mailto-link to write email to the author</span>
-                    </a>
-                </li>
-            </ul>
-        </div>
-    }
-}
+const Socials = () => (
+<div className="socials">
+    <ul className="icons">
+        <li>
+            <a href="https://github.com/Jammar" className="fa fa-github" alt="Link to github page">
+                <span className="label">Link to github page</span>
+            </a>
+        </li>
+        <li>
+            <a href="mailto:joachim@hideaddress.net" className="fa fa-at" alt="Mailto-link to author">
+                <span className="label">Mailto-link to write email to the author</span>
+            </a>
+        </li>
+    </ul>
+</div>
+)
 
 export default Socials
--- src/pages/joachim.js
+++ src/pages/joachim.js
@@ -6,28 +6,24 @@ import NavBar from "../components/NavBar"
 import avatar from "../assets/images/profo-el-pictuehero.png"
 import Socials from "../components/Socials"
 
-const IndexPage = () => (
+const JoachimPage = () => (
   <Layout>
     <SEO title="Joachim" />
     <NavBar />
-  <div className="content">
-    <h1>This is Joachim</h1>
-    <img className="avatar" src={avatar} alt="" />
-    <div className="ingress">
-      A Bachelor of Computer Science with <span>a focus on interaction design</span>.
+    <div className="content">
+      <h1>This is Joachim</h1>
+      <img className="avatar" src={avatar} alt="" />
+      <div className="ingress">
+        A Bachelor of Computer Science with <span>a focus on interaction design</span>.
+      </div>
+      < br/><br/>
+      <p>
+        I like to think, discuss and criticise UX as well as usability and accessability. But I also really like to code.
+        Welcome to my simple page!
+      </p>
     </div>
-    < br/><br/>
-    <p>
-      I like to think, discuss and criticise UX as well as usability and accessability. But I also really like to code.
-      Welcome to my simple page!
-    </p>
-    {/* <p>
-        See <Link to="/projects/">PROJECTS</Link> for my work experience or click <Link to="/about/">ABOUT</Link> to read 
-        more about how I made this site and my thought process designing it.
-    </p> */}
-  </div>
-  <Socials />
+    <Socials />
   </Layout>
 )
 
-export default IndexPage
+export default JoachimPage
