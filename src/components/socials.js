2047cad925ce18b0f76d2f69d1ee32129e6dd3d7
Jammar <46968969+Jammar@users.noreply.github.com>
2020-02-25 11:40:44 +0100

    try deploying Socials

--- src/components/socials.js
+++ src/components/socials.js
@@ -2,21 +2,23 @@ import React from "react"
 import { IoLogoGithub } from 'react-icons/io';
 import { MdEmail } from 'react-icons/md';
 
-const Socials = () => (
-    <div className="socials">
-        <ul className="icons">
-            <li>
-                <a href="https://github.com/Jammar">
-                    <IoLogoGithub />
-                </a>
-            </li>
-            <li>
-                <a href="mailto:joachim@hideaddress.net">
-                    <MdEmail />
-                </a>
-            </li>
-        </ul>
-    </div>
-)
+class Socials extends React.Component {
+    render() {
+        return <div className="socials">
+            <ul className="icons">
+                <li>
+                    <a href="https://github.com/Jammar">
+                        <IoLogoGithub />
+                    </a>
+                </li>
+                <li>
+                    <a href="mailto:joachim@hideaddress.net">
+                        <MdEmail />
+                    </a>
+                </li>
+            </ul>
+        </div>
+    }
+}
 
 export default Socials
