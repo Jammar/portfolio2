c832f7a49e75fbd2a717bd0a6850c89d580d643e
Jammar <46968969+Jammar@users.noreply.github.com>
2020-02-26 13:14:44 +0100

    404 page updated

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,20 @@ import React from "react"
 
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import MainMenu from "../components/NavBar"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a page that doesn&#39;t exist. Try the menu to find one that does :)</p>
+    <div className="welcome">
+      <h1>NOT FOUND</h1>
+    </div>
+    <div className="content">
+      <p className="ingress">
+        You just hit a page that doesn&#39;t exist. Try one of these:
+      </p>
+    </div>
+    <MainMenu />
   </Layout>
 )
 
